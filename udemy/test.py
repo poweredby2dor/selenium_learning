2a14b7ecb202f70f11bc99703a07e6925a6729d7
2dor <poweredby2dor@gmail.com>
2024-11-06 14:05:46 +0200

    Prepared Udemy folder

---
+++ udemy/test.py
@@ -0,0 +1,17 @@
+"""
+******************************************************************************
+CONFIDENTIAL AND PROPRIETARY
+
+This source code is the property of [Your Company Name] and is provided
+under the terms of a Non-Disclosure Agreement (NDA) between [Your Company
+Name] and the authorized recipient. Any unauthorized copying, distribution,
+modification, or use of this source code is strictly prohibited and may
+result in legal action.
+
+© [Year] [Your Company Name]. All rights reserved.
+******************************************************************************
+"""
+
+import time
+
+time.sleep(1)
